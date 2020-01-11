13be794adb812267331a553fa5071e9797c41e9e
vinay thallam <vinay.rgit@gmail.com>
2020-01-11 11:47:42 +0530

    long test task story

--- src/components/Task.stories.js
+++ src/components/Task.stories.js
@@ -37,3 +37,9 @@ export const Pinned = () => <Task task={{ ...taskData, state: 'TASK_PINNED' }} {
 export const Archived = () => (
   <Task task={{ ...taskData, state: 'TASK_ARCHIVED' }} {...actionsData} />
 );
+
+const longTitleString = `This task's name is absurdly large. In fact, I think if I keep going I might end up with content overflow. What will happen? The star that represents a pinned task could have text overlapping. The text could cut-off abruptly when it reaches the star. I hope not!`;
+
+export const LongTitle = () => (
+  <Task task={{ ...taskData, title: longTitleString }} {...actionsData} />
+);
