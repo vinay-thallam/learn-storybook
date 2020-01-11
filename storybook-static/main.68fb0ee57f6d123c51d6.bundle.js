073c1b29b14c1ad8d7356d5c21a6ca3431de86ba
vinay thallam <vinay.rgit@gmail.com>
2020-01-11 12:28:07 +0530

    built story book as a static web app

---
+++ storybook-static/main.68fb0ee57f6d123c51d6.bundle.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{0:function(n,o,c){c(499),c(690),n.exports=c(1066)},1:function(n,o){},690:function(n,o,c){"use strict";c.r(o);c(691),c(954),c(1063)}},[[0,1,2]]]);
---
+++ storybook-static/precache-manifest.358825bb38c1133c018c90fd6f72a64c.js
@@ -0,0 +1,19 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58cfb658ebc0a23bdfc174ef7b635b50",
+    "url": "iframe.html"
+  },
+  {
+    "revision": "ff7f103191500d052c4e",
+    "url": "main.css"
+  },
+  {
+    "url": "main.ffad1fbb915c5aa1d4ab.bundle.js"
+  },
+  {
+    "url": "runtime~main.ffad1fbb915c5aa1d4ab.bundle.js"
+  },
+  {
+    "url": "vendors~main.ffad1fbb915c5aa1d4ab.bundle.js"
+  }
+]);
